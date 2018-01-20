e435c3e9b248b0b8d2e9f75a78dcd7b4eb20e249
PatriciaCabrero <patrcabr@ucm.es>
2018-01-20 14:47:44 +0100

    EJ32 y EJ37

--- EJ00/EJ32-6Grados/EJ32.cpp
+++ EJ00/EJ32-6Grados/EJ32.cpp
@@ -1,93 +1,95 @@
-// VJ04, Patricia Cabrero Villar
+﻿// Grupo VJ04, Patricia Cabrero Villar
 /*
-SEIS GRADOS DE SEPARACIÓN
+	Se utiliza el algoritmo de Floyd para la búsqueda de los caminos mínimos entre dos puntos.
+	La recursion utilizada es:
+					0 si i = j
+	G[i, j] =		coste si hay arista de i a j
+					+∞ si no hay arista de i a j
 
-                    tabla(i-1,j-1)+1   si P1i == P2j
- tabla(i,j) =
-                    max(tabla(i-1,j), tabla(i,j - 1) si i<n y j< m
- */
+	COSTE
+		C^k(k, j) = mín{Ck−1(k, j),Ck−1(k, k) + Ck−1(k, j)}
+*/
+
 #include <iostream>
-#include <iomanip>
 #include <fstream>
 #include <vector>
-#include <assert.h>
 #include <stdio.h>
 #include <algorithm>
-#include <map>
+#include <string>
+#include <unordered_map>
 #include "Matriz.h"
-#include "Grafo.h"
-using namespace std;
 
+using namespace std;
 const int INF = 1000000000;
-// COSTE O(n^2) en tiempo y O(2n) en espacio
-int Floyd(Matriz<int> const& G, Matriz<int> & C,
-           Matriz<size_t> & camino) {
-    size_t n = G.numfils() - 1;
-    // inicialización
-    C = G;
-    camino = Matriz<size_t>(n+1,n+1,0);
-    // actualizaciones de la matriz
-    for (size_t k = 1; k <= n; ++k)
-        for (size_t i = 1; i <= n; ++i)
-            for (size_t j = 1; j <= n; ++j) {
-                int temp = C[i][k] + C[k][j];
-                if (temp < C[i][j]) { // es mejor pasar por k
-                    C[i][j] = temp;
-                    camino[i][j] = k;
-                }
-            }
-    int valor = 0;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j < n; j++) {
-            if(valor<C[i][j])
-                valor=C[i][j];
-        }
-    }
-    return valor;
+// función que resuelve el problema
+//Θ(n^3) en tiempo y Θ(1) en espacio adicional.
+void Floyd( Matriz<int> & C) {
+	size_t n = C.numfils()- 1;
+	// inicialización
+	// actualizaciones de la matriz
+	for (size_t k = 1; k <= n; ++k)
+		for (size_t i = 1; i <= n; ++i)
+			for (size_t j = 1; j <= n; ++j) {
+				int temp = C[i][k] + C[k][j];
+				if (temp < C[i][j]) { // es mejor pasar por k
+					C[i][j] = temp;
+				}
+			}
+
+}int buscaMax(Matriz<int> & C){
+	int max = 0;
+	for (size_t i = 1; i < C.numcols(); i++)
+	{
+		for (size_t j = i; j < C.numcols(); j++)
+		{
+			if (C[i][j] > max) max = C[i][j];
+		}
+	}
+	return max;
 }
-// Resuelve un caso de prueba, leyendo de la entrada la
-// configuración, y escribiendo la respuesta
 
 bool resuelveCaso() {
-    size_t V;
-    cin >> V;
-    if (!cin) return false;
-    size_t E;
-    cin >> E;
-    
-    Matriz<int> grafo(V+1,V+1,INF);
-    for (size_t i = 1; i <= V; ++i)
-        grafo[i][i] = 0;
-    size_t u, v;
-    for (size_t i = 1; i <= E; ++i) { // leer aristas
-        cin >> u >> v ;
-        grafo[u][v] = 1;
-        grafo[v][u] = 1;
-    }
-    Matriz<int> C(0,0); Matriz<size_t> camino(0,0);
-    int sol = Floyd(grafo, C, camino);
-    if(sol==0)cout<<"DESCONECTADO"<<endl;
-    else cout<<sol<< endl;
-    //imprimir_caminos(C, camino);
-    return true;
+	size_t V; 
+	cin >> V;
+	if (!cin) return false;
+	size_t E; cin >> E;
+	Matriz<int> grafo(V + 1, V + 1, INF);
+	for (size_t i = 1; i <= V; ++i)
+		grafo[i][i] = 0;
+	string u, v;
+	int identificador=1;
+	std::unordered_map<string, int> amigos;
+	for (size_t i = 1; i <= E; ++i) { // leer aristas
+		std::cin >> u >> v;
+		auto aux = amigos.insert(std::pair<string, int>{ u, identificador });
+		if (aux.second)
+			identificador++;
+		auto aux1 = amigos.insert(std::pair<string, int>{ v, identificador });
+		if (aux1.second)
+			identificador++;
+		grafo[aux.first->second][aux1.first->second] = 1;
+		grafo[aux1.first->second][aux.first->second] = 1;
+
+	}
+	Floyd(grafo);
+	int sol = buscaMax(grafo);
+	if (sol<INF)
+		std::cout << sol << endl;
+	else
+		std::cout << "DESCONECTADA" << endl;
+
+	return true;
 }
 
 int main() {
-    // Para la entrada por fichero.
-    // Comentar para acepta el reto
 #ifndef DOMJUDGE
-    std::ifstream in("/Users/Pac/Documents/Universidad/UCM 3º/MARP/EJ00/EJ32-6Grados/datos32.txt");
-    auto cinbuf = std::cin.rdbuf(in.rdbuf()); //save old buf and redirect std::cin to casos.txt
+	std::ifstream in("datos33.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
 #endif
-    
-    while (resuelveCaso())
-        ;
-    
-    // Para restablacer entrada. Comentar para acepta el reto
-#ifndef DOMJUDGE // para dejar todo como estaba al principio
-    std::cin.rdbuf(cinbuf);
-    system("PAUSE");
+	while (resuelveCaso());
+#ifndef DOMJUDGE
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
 #endif
-    
-    return 0;
+	return 0;
 }
